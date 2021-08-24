b8303f124c538fd9048716bb8f4a131be63914c6
Derek Anderson <dmikey@users.noreply.github.com>
2021-08-24 14:57:32 -0500

    convert server over to fastify instead of express (#8)

--- app.ts
+++ app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import jwt from "express-jwt";
 import jwksRsa from "jwks-rsa";
 import cors from "cors";
-import logger from "morgan";
+import fastify from "fastify";
+import fastifyExpress from "fastify-express";
 
 import { User } from "./database";
 
@@ -12,40 +13,47 @@ import { router as usersRouter } from "./routes/users";
 import { router as transactionsRouter } from "./routes/transactions";
 import { router as blockedAddressesRouter } from "./routes/blocked-addresses";
 
-var app = express();
+async function init() {
+  // var app = express();
+  const app = fastify({
+    logger: true,
+  });
 
-const DOMAIN = process.env.AUTH0_DOMAIN;
-const AUDIENCE = process.env.AUTH0_AUDIENCE;
+  const DOMAIN = process.env.AUTH0_DOMAIN;
+  const AUDIENCE = process.env.AUTH0_AUDIENCE;
 
-const checkJwt = jwt({
-  secret: jwksRsa.expressJwtSecret({
-    jwksUri: `https://${DOMAIN}/.well-known/jwks.json`,
-  }),
-  audience: AUDIENCE,
-  issuer: `https://${DOMAIN}/`,
-  algorithms: ["RS256"],
-  credentialsRequired: false,
-});
+  const checkJwt = jwt({
+    secret: jwksRsa.expressJwtSecret({
+      jwksUri: `https://${DOMAIN}/.well-known/jwks.json`,
+    }),
+    audience: AUDIENCE,
+    issuer: `https://${DOMAIN}/`,
+    algorithms: ["RS256"],
+    credentialsRequired: false,
+  });
 
-async function loadUser(req: any, res: any, next: any) {
-  if (req.user) {
-    let user = await User.findOne({ where: { sub: req.user.sub } });
-    if (user) req.user = { ...req.user, ...(user as any).dataValues };
+  async function loadUser(req: any, res: any, next: any) {
+    if (req.user) {
+      let user = await User.findOne({ where: { sub: req.user.sub } });
+      if (user) req.user = { ...req.user, ...(user as any).dataValues };
+    }
+    return next();
   }
-  return next();
+
+  await app.register(fastifyExpress);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors()); // Allow all cors (not recommended for production)
+  app.use(checkJwt);
+  app.use(loadUser);
+
+  app.use("/", indexRouter);
+  app.use("/faucet", faucetRouter);
+  app.use("/users", usersRouter);
+  app.use("/transactions", transactionsRouter);
+  app.use("/blocked-addresses", blockedAddressesRouter);
+
+  return app;
 }
 
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cors()); // Allow all cors (not recommended for production)
-app.use(checkJwt);
-app.use(loadUser);
-
-app.use("/", indexRouter);
-app.use("/faucet", faucetRouter);
-app.use("/users", usersRouter);
-app.use("/transactions", transactionsRouter);
-app.use("/blocked-addresses", blockedAddressesRouter);
-
-export { app };
+export { init };
