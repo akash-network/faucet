6e5c342f29b57387e896078606a0df0efd869693
Derek Anderson <derek@akash.network>
2022-01-27 07:19:41 -0600

    add auth and psql as opts

--- routes/faucet.ts
+++ routes/faucet.ts
@@ -12,10 +12,9 @@ import {
 } from "../utils";
 
 const DOMAIN = process.env.AUTH0_DOMAIN;
-const ACCOUNT_AGE_MIN_DAYS: number =
-  parseInt(process.env.ACCOUNT_AGE_MIN_DAYS as string, 10) || 90;
 
 import client from "prom-client";
+import e from "cors";
 
 const counterDrip = new client.Counter({
   name: "faucet_transaction_count",
@@ -27,49 +26,46 @@ const counterDripError = new client.Counter({
   help: "faucet_transaction_count is the number of times the faucet errored while dripping",
 });
 
-function isASCII(str: string) {
-  return /^[\x00-\x7F]*$/.test(str);
-}
+const noop = (req: any, res: any, next: any) => next();
 
 router.post(
   "/",
-  ensureAuthenticated,
-  decorateGithubUser,
-  blockedAddresses,
-  rateLimit,
+  DOMAIN ? ensureAuthenticated : noop,
+  DOMAIN ? decorateGithubUser : noop,
+  DOMAIN ? blockedAddresses : noop,
+  DOMAIN ? rateLimit : noop,
   async (req: any, res: any, next: any) => {
     const { address } = req.body;
 
     try {
-      if (req.user.github && isASCII(req.user.github.created_at)) {
-        const oneDayMilliseconds = 8.64e7;
+      if (DOMAIN && req.user?.github) {
+        const oneQuarter = 7.776e9;
         const dateSince =
           new Date().getTime() - new Date(req.user.github.created_at).getTime();
 
-        if (dateSince < ACCOUNT_AGE_MIN_DAYS * oneDayMilliseconds) {
+        if (dateSince < oneQuarter) {
           // user account is under 90 days
           res.status(422).send(
             JSON.stringify({
               error: "social account is not mature enough to request funds.",
             })
           );
-          return; // stop processing tx
         }
-      } else {
+      } else if (DOMAIN) {
         //throw we need a user account
         counterDripError.inc();
         res
           .status(422)
           .send(JSON.stringify({ error: "social account not located." }));
-        return; // stop processing tx
       }
 
-      if (!req.user.id) {
+      if (DOMAIN && !req.user?.id) {
         const { body } = await got(`https://${DOMAIN}/userinfo`, {
           headers: { authorization: req.headers.authorization },
           responseType: "json",
         });
         let { nickname, name, email, picture } = body as any;
+
         let user: any = await User.create({
           sub: req.user.sub,
           nickname,
@@ -79,17 +75,26 @@ router.post(
         });
         req.user = Object.assign(req.user, user.dataValues);
       }
-      let transaction = await Transaction.create({
-        userId: req.user.id,
-        address: address,
-        amount: faucet.getDistributionAmount(),
-      });
-      const result = await faucet.sendTokens(address, null);
-      transaction.update({ transactionHash: result.transactionHash });
+
+      if (process.env.POSTGRES_HOST) {
+        let transaction = await Transaction.create({
+          userId: req.user.id,
+          address: address,
+          amount: faucet.getDistributionAmount(),
+        });
+        const result = await faucet.sendTokens(address, null);
+        transaction.update({ transactionHash: result.transactionHash });
+        res
+          .status(201)
+          .send(JSON.stringify({ transactionHash: result.transactionHash }));
+      } else {
+        const result = await faucet.sendTokens(address, null);
+        res
+          .status(201)
+          .send(JSON.stringify({ transactionHash: result.transactionHash }));
+      }
+
       counterDrip.inc();
-      res
-        .status(201)
-        .send(JSON.stringify({ transactionHash: result.transactionHash }));
     } catch (error) {
       counterDripError.inc();
       res.status(422).send(JSON.stringify({ error: error.message }));
